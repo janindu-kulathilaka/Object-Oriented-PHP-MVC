49637d212c992a92fce6fcc01fbd43f94fc424dd
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-10-27 13:08:05 +0530

    inheritance

---
+++ 2_6.php
@@ -0,0 +1,40 @@
+<?php
+class User
+{
+  protected $name;
+  protected $age;
+
+  public function __construct($name, $age)
+  {
+    $this->name = $name;
+    $this->age = $age;
+  }
+}
+
+
+class Customer extends User
+{
+
+  private $balance;
+
+  public function __construct($name, $age, $balance)
+  {
+    parent::__construct($name, $age);
+    $this->balance = $balance;
+  }
+  public function pay($amount)
+  {
+    return $this->name . " paid $" . $amount;
+  }
+
+  public function getBalance()
+  {
+    return $this->balance;
+  }
+}
+
+$customer1 = new Customer("Janindu", 21, 500);
+
+echo $customer1->pay(110);
+
+echo "<br>" . $customer1->getBalance();
